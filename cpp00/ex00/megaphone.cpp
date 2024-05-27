f524123cb7ff251e91db1fe29288e827ab67d4bd
angelasoler <manzanaembicicleta@gmail.com>
2024-05-27 16:31:33 -0300

    add c++

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -4,14 +4,13 @@ int	main(int argc, char *argv[])
 {
 	if (argc == 1)
 	{
-		std::cout << "SCREAAAAAM" << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (1);
 	}
 	for (int nwords = 1; nwords < argc; nwords++)
 	{
 		for (int i = 0; argv[nwords][i]; i++)
 			std::cout << (char)std::toupper(argv[nwords][i]);
-		std::cout << " ";
 	}
 	std::cout << std::endl;
 	return (0);
